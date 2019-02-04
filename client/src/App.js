c146ca56ca095cacf79c2b28481c5787794172e7
melanoman <melanoman@gmail.com>
2019-02-04 11:17:45 -0800

    Added arrows to lesson chooser

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencilAlt,faChalkboardTeacher,faReply } from '@fortawesome/free-solid-svg-icons'
+import { faPencilAlt,faChalkboardTeacher,faReply, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faPencilAlt);
 library.add(faChalkboardTeacher);
 library.add(faReply);
+library.add(faArrowLeft);
+library.add(faArrowRight);
 
 const start_mode = 0;
 const edit_mode = 1;
@@ -37,8 +39,8 @@ class App extends Component {
           {
             this.state.mode === start_mode ? (
               <div>
-                <button onClick={() => this.edit_mode()}><FontAwesomeIcon icon="pencil-alt" size = "8x" /></button>
-                <button onClick={() => this.quiz_mode()}><FontAwesomeIcon icon="chalkboard-teacher" size = "8x" /></button>
+                <button className="inline" onClick={() => this.edit_mode()}><FontAwesomeIcon icon="pencil-alt" size = "8x" /></button>
+                <button className="inline" onClick={() => this.quiz_mode()}><FontAwesomeIcon icon="chalkboard-teacher" size = "8x" /></button>
               </div>
             ) : (this.state.mode === edit_mode) ? (
               <div>
@@ -68,21 +70,37 @@ class EditScreen extends Component {
 
   newLesson(event) {
     var val = parseInt(event.target.value);
-    if (!isNaN(val)) { this.setState({lesson: val}); }
+    if (!isNaN(val) && val > 0) { this.setState({lesson: val}); }
   }
 
   render() {
     return (<div>
-      <div> Lesson <input
-        type="text" placeholder={this.props.lesson} size="3"
-        value={this.props.lesson} onInput={(event)=>{this.newLesson(event)}}
-      / ></div>
-      <div>Editing lesson {this.state.lesson}</div>
+      <LessonChooser parent={this} />
       <p>Table goes here</p>
     </div>);
   }
 }
 
+class LessonChooser extends Component {
+
+  incr() { this.props.parent.setState({lesson: (this.props.parent.state.lesson + 1)}); }
+  decr(){ this.props.parent.setState({lesson: (this.props.parent.state.lesson - 1)}); }
+
+  render() {
+    return (<div>
+      <button className="inline" onClick={()=>this.decr()}><FontAwesomeIcon icon="arrow-left" size = "4x" /></button>
+      <span className="inline">
+        <div>Lesson <input size="3" type="text"
+           placeholder={this.props.parent.state.lesson}
+           value={this.props.parent.state.lesson}
+           onInput={(event)=>{this.props.parent.newLesson(event)}}
+        / ></div>
+        <div>Editing lesson {this.props.parent.state.lesson}</div>
+      </span>
+      <button className="inline" onClick={()=>this.incr()}><FontAwesomeIcon icon="arrow-right" size = "4x" /></button>
+    </div>);
+  }
+}
 class QuizScreen extends Component {
   render () {
     return (
