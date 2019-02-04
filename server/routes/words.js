28d8a2b5783a6dc8406c7f7082218e14f506b53f
melanoman <melanoman@gmail.com>
2019-02-04 14:07:37 -0800

    Placeholder for insert into dictionary command

--- server/routes/words.js
+++ server/routes/words.js
@@ -10,8 +10,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:lesson', function(req, res, next) {
+router.get('/lesson/:lesson', function(req, res, next) {
   var query = 'select * from questions where lesson = ' + req.params.lesson;
+  conn.query(query, (err, data) => {
+    (err)?res.send(err):res.json({words: data});
+  });
+});
+
+/* Shouldn't use get, but quicker to manually test for now. */
+router.get('/new/:lesson/:word/:kanji/:reading', function(req, res, next) {
+  var query = "insert into questions (lesson, word, kanji, reading) values (" +
+              req.params.lesson + ",'" +
+              req.params.word + "','" +
+              req.params.kanji + "','" +
+              req.params.reading + "')";
   conn.query(query, (err, data) => {
     (err)?res.send(err):res.json({words: data});
   });
